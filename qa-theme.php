380d8f1c49a4b4c692c80f64ae5a99e0bccd6dd6
NoahY <noahy57@gmail.com>
2011-10-09 12:38:45 +0530

    site title

--- qa-theme.php
+++ qa-theme.php
@@ -5,6 +5,7 @@
 		{
 			$this->content['navigation']['footer']['feedback']['label'] = '<div class="feedback-image"></div>';
 			$this->content['search']['button_label'] = '';
+			$this->content['logo'] = '<A HREF="../" CLASS="qa-logo-link">'.qa_opt('site_title').'</A>';
 
 			qa_html_theme_base::doctype();
 		}
